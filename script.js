d60225914a820ab5870b47c865351fb1a3bf7389
rameen <khan.rameen772@gmail.com>
2025-12-03 14:54:11 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,52 @@
+//loonie
+function calculateLoonieTotal() {
+  let total = document.getElementById("userInput1").value;
+  total = parseFloat(total);
+  
+  let silverPrice = document.getElementById("content1").innerText;
+  silverPrice = parseFloat(silverPrice);
+
+  let totalPrice = total*silverPrice;
+
+  document.getElementById("looniePrice").innerText = "$" + totalPrice.toFixed(2);
+}
+
+//fifty cents
+function calculateFiftyTotal() {
+  let total = document.getElementById("userInput2").value;
+  total = parseFloat(total);
+
+  let silverPrice = document.getElementById("content2").innerText;
+  silverPrice = parseFloat(silverPrice);
+
+  let totalPrice = total*silverPrice;
+
+  document.getElementById("fiftyPrice").innerText = "$" + totalPrice.toFixed(2);
+}
+
+//quarter
+function calculateQuarterTotal() {
+  let total = document.getElementById("userInput3").value;
+  total = parseFloat(total);
+
+  let silverPrice = document.getElementById("content3").innerText;
+  silverPrice = parseFloat(silverPrice);
+
+  let totalPrice = total*silverPrice;
+
+  document.getElementById("quarterPrice").innerText = "$" + totalPrice.toFixed(2);
+}
+
+//dime
+function calculateDimeTotal() {
+  let total = document.getElementById("userInput4").value;
+  total = parseFloat(total);
+
+  let silverPrice = document.getElementById("content4").innerText;
+  silverPrice = parseFloat(silverPrice);
+
+  let totalPrice = total*silverPrice;
+
+  document.getElementById("dimePrice").innerText = "$" + totalPrice.toFixed(2);
+}
+
